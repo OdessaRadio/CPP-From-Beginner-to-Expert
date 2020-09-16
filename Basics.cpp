72f2b949a5cd194ec97f96c89d2553661bec6849
OdessaRadio <alexander_lyashko@hotmail.com>
2020-09-16 16:16:52 +0300

    Basics. Exercise 1

--- Basics.cpp
+++ Basics.cpp
@@ -1,10 +1,12 @@
-# include <iostream>
-using namespace std;
+
+#include <iostream> //This console output is supposed to show the result of 2+2 equation
+
+//using namespace std; //This way you have access to whole std namespace throughout your code.
+//using std::cout; //This way you have access only to cout throughout your code.
+//std::cout; //This way you are accessing the cout only at the line it is used on.
 
 main()
 {
-    cout << "This is test\n";
-    // cout is Console Output
-    // << is direction pointer
-    // "" is for strings
+    std::cout << "The evaluation of 2+2 is equal to 4";
+	//In the line above we are attaching iostream (standard input/output) 	library which is necessary for using cout (console output)
 }
