97d8a3bb08bc8b4313c6e66fde550bc512836af9
OdessaRadio <alexander_lyashko@hotmail.com>
2020-09-17 13:22:03 +0300

    Downloading data from the input

--- Basics.cpp
+++ Basics.cpp
@@ -4,22 +4,18 @@
 
 main()
 {
-    // Ex.1
-    char a('A');   //character 1 byte -128 to 127
-    int b(2000);    //integer 4 bytes -2147483648 to 2147483647
-    short c(-32768);  //short integer 2 bytes -32768 to 3276
-    float d = 27.6787651232323231312;  //floating point 4 bytes +/- 3.4e +/- 38 (~7 digits)
-    double f = 27.6787651232323231312; //double floating point 8 bytes +/- 1.7e +/- 308 (~15 digits)
-    // Ex.2
-    std::cout << "variable a value is: " << a << "\n";
-    std::cout << "variable b value is: " << b << "\n";
-    std::cout << "variable c value is: " << c << "\n";
-    std::cout << "variable d value is: " << d << "\n";
-    std::cout << "variable f value is: " << f << "\n";
-    // Ex. 3
-    int room_Number;
-    unsigned floorLevel; // unsigned char
-    string firstName, lastName;
-    bool mealsIncluded;
+//   //std::cin cin - console input
+//    char a;
+//    std::cin >> a; // console input
+//
+//    std::cout << "Variable a = " << a << "\n";
+    std::string name, surname;
+    std::cout << "Enter your name: ";
+    std::cin >> name; // reading input from console
+    std::cout << "Enter your surname: ";
+    std::cin >> surname; // reading input from console
 
+    std::cout << name << " " << surname << std::endl;
+
+    system("pause");
 }
