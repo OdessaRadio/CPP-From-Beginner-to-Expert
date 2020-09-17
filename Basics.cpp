e86dbd4e2f95b2fe8fd8fd3980bc3ba7e1c641f1
OdessaRadio <alexander_lyashko@hotmail.com>
2020-09-17 12:43:25 +0300

    Types of variables. Exercises

--- Basics.cpp
+++ Basics.cpp
@@ -1,52 +1,25 @@
-
 #include <iostream>
 
-using namespace std;
+//using namespace std;
 
 main()
 {
-    int a = 10, b = 0, c = 30; // allocating 4 bytes
-    cout << "a : " << a << " Address is: " << &a <<endl;
-    cout << "b : " << b << " Address is: " << &b <<endl;
-    cout << "c : " << c << " Address is: " << &c <<endl;
-
-    short int t1;       // allocating 2 bytes
-    //short t1;         // the same as short int
-
-    t1 = 5;
-    cout << t1 << endl;
-
-    float t2 = 5.12;    // 4 bytes of memory
-    double t3 = 10.12;  // 8 bytes of memory. double float
-    cout << t2 << endl;
-    cout << t3 << endl;
-
-    char t4;    // character
-    t4 = 'a';
-    cout << t4 << endl;
-
-    string t5 = "Hello World!\n";
-    cout << t5 << endl;
-
-    string x = "part1";
-    string y = "part2";
-    string combineStrings = x+y;
-    cout << combineStrings << endl;
-
-    // boolean True or False
-    bool t6 = true;
-    cout << t6 << endl;
-
-    unsigned short int t7 = 54000; // 2 bytes
-    cout << t7 << endl;
-
-    const string NAME_OF_GAME = "C++"; // const means we can't change it
-    cout << NAME_OF_GAME << endl; // const variable names are in capital letters
-
-    cout << "\n" << "Practicing the calculations" << "\n";
-    cout << "5/2= " << 5/2 << "\n";
-    cout << "5.0/2= " << 5.0/2 << "\n";
-
-
+    // Ex.1
+    char a('A');   //character 1 byte -128 to 127
+    int b(2000);    //integer 4 bytes -2147483648 to 2147483647
+    short c(-32768);  //short integer 2 bytes -32768 to 3276
+    float d = 27.6787651232323231312;  //floating point 4 bytes +/- 3.4e +/- 38 (~7 digits)
+    double f = 27.6787651232323231312; //double floating point 8 bytes +/- 1.7e +/- 308 (~15 digits)
+    // Ex.2
+    std::cout << "variable a value is: " << a << "\n";
+    std::cout << "variable b value is: " << b << "\n";
+    std::cout << "variable c value is: " << c << "\n";
+    std::cout << "variable d value is: " << d << "\n";
+    std::cout << "variable f value is: " << f << "\n";
+    // Ex. 3
+    int room_Number;
+    unsigned floorLevel; // unsigned char
+    string firstName, lastName;
+    bool mealsIncluded;
 
 }
