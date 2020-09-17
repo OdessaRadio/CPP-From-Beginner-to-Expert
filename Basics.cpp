49ad6ed5abb9fb8e30ae755427cad3c2945ce112
OdessaRadio <alexander_lyashko@hotmail.com>
2020-09-17 13:38:34 +0300

    Downloading data from the input. Exercises

--- Basics.cpp
+++ Basics.cpp
@@ -4,18 +4,30 @@
 
 main()
 {
-//   //std::cin cin - console input
-//    char a;
-//    std::cin >> a; // console input
-//
-//    std::cout << "Variable a = " << a << "\n";
-    std::string name, surname;
-    std::cout << "Enter your name: ";
-    std::cin >> name; // reading input from console
-    std::cout << "Enter your surname: ";
-    std::cin >> surname; // reading input from console
+    // Ex.1
+    std::string string1, string2, string3;
+    std::cout << "Enter string1: \n";
+    std::cin >> string1;
+    std::cout << "Enter string2: \n";
+    std::cin >> string2;
+    std::cout << "Enter string3: \n";
+    std::cin >> string3;
 
-    std::cout << name << " " << surname << std::endl;
+    std::cout << "The combination of entered strings is:\n";
+    std::cout << string1 << " " << string2 << " " << string3 << std::endl;
 
     system("pause");
+
+    //Ex.2
+    int number1, number2, number3;
+    std::cout << "Enter integer 1: ";
+    std::cin >> number1;
+    std::cout << "Enter integer 2: ";
+    std::cin >> number2;
+    number3 = number1;
+    number1=number2;
+    number2=number3;
+    std::cout << "Number1 = " << number1 << std::endl;
+    std::cout << "Number2 = " << number2;
+
 }
