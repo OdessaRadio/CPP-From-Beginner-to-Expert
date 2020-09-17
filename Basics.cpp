7fa2d0024107aa33fbf130fcdad82eed7b789259
OdessaRadio <alexander_lyashko@hotmail.com>
2020-09-17 10:47:20 +0300

    Basics of programming. Exercise

--- Basics.cpp
+++ Basics.cpp
@@ -1,12 +1,13 @@
 
 #include <iostream> //This console output is supposed to show the result of 2+2 equation
 
-//using namespace std; //This way you have access to whole std namespace throughout your code.
-//using std::cout; //This way you have access only to cout throughout your code.
-//std::cout; //This way you are accessing the cout only at the line it is used on.
+//using namespace std;  //access to whole std namespace throughout your code.
+//using std::cout;      //access only to cout throughout your code.
+//std::cout;            //accessing the cout only at the line it is used on.
 
 main()
 {
-    std::cout << "The evaluation of 2+2 is equal to 4";
+    std::cout << "The evaluation of 2+2 is equal to 4\n";
 	//In the line above we are attaching iostream (standard input/output) 	library which is necessary for using cout (console output)
+	std::cout << "This is my first stream\n";
 }
