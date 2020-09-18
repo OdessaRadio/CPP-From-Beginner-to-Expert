83c6263bf088f7dbb6dc636360024c711b9dff2f
OdessaRadio <alexander_lyashko@hotmail.com>
2020-09-18 12:44:19 +0300

    Relational operators

--- Operators.cpp
+++ Operators.cpp
@@ -1,86 +1,19 @@
 # include <iostream>
 
 using namespace std;
+//relational operators
 
 main()
 {
-/*
-    //Ex.1
+    int a = 9;
+    int b = 10;
 
-    float i;
-
-    cout << "Inches to centimeter converter" << "\n" ;
-    cout << "Enter length in inches: ";
-    cin >> i;
-    cout << i << " inch(es) is " << i*2.54 << " in centimeters\n";
-
-    system ("pause");
-
-*/
-/*
-    //Ex.2
-
-    float celsiusTemp;
-
-    cout << "Celsius to Kelvin, Fahrenheit converter\n";
-    cout << "Enter temperature value in Celcius: ";
-    cin >> celsiusTemp;
-    cout << celsiusTemp << " by Celsius is:\n";
-    cout << celsiusTemp+273.15 << " by Kelvin\n";
-    cout << celsiusTemp*1.8 + 32 << " by Fahrenheit" << endl;
-
-    system ("pause");
-*/
-/*
-    //Ex.3
-    float number1, number2;
-    cout << "Enter number 1: \n";
-    cin >> number1;
-    cout << "Enter number 2: \n";
-    cin >> number2;
-    cout << number1 << " + " << number2 << " = " << number1 + number2 << endl;
-
-    system ("pause");
-*/
-/*
-    //Ex.4
-    float number3, number4;
-    cout << "Enter number 3: \n";
-    cin >> number3;
-    cout << "Enter number 4: \n";
-    cin >> number4;
-    cout << number3 << " - " << number4 << " = " << number3 - number4 << endl;
-
-    system ("pause");
-*/
-/*
-    //Ex.5
-    float number5, number6;
-    cout << "Enter number 5: \n";
-    cin >> number5;
-    cout << "Enter number 6: \n";
-    cin >> number6;
-    cout << number5 << " * " << number6 << " = " << number5 * number6 << endl;
-    system ("pause");
-*/
-/*
-    //Ex.6
-    float number7, number8;
-    cout << "Enter number 7: \n";
-    cin >> number7;
-    cout << "Enter number 8: \n";
-    cin >> number8;
-    cout << number7 << " / " << number8 << " = " << number7 / number8 << endl;
-    system ("pause");
-*/
-
-    //Ex.7
-    int number9, number10;
-    cout << "Enter number 9: \n";
-    cin >> number9;
-    cout << "Enter number 10: \n";
-    cin >> number10;
-    cout << number9 << " % " << number10 << " = " << number9 % number10 << endl;
-    system ("pause");
+    cout << (a == b) << endl; //== represent equality
+    cout << (a != b) << endl; //!= is not equel
+    cout << (a > b) << endl; //
+    cout << (a < b) << endl; //
+    cout << (a >= b) << endl; //
+    cout << (a <= b) << endl; //
+    cout << !(a == b) << endl
 
 }
