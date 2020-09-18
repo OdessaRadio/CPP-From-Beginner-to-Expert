fc93849772139e4bf6b286e0a026fe4f198ac734
OdessaRadio <alexander_lyashko@hotmail.com>
2020-09-18 10:27:14 +0300

    Arithmetic and Assignment Operators

---
+++ Operators.cpp
@@ -0,0 +1,47 @@
+# include <iostream>
+
+using namespace std;
+
+main()
+{
+    /*
+    int a = 10;
+    int b = 4;
+
+    cout << a + b << endl;  // addition operator
+    cout << a - b << endl;  // substraction operator
+    cout << a * b << endl;  // multiplication
+    cout << a / b << "\n";  // devision
+    // 10 % 4 = 2 - remainder  10 - 8 = 2 // modulo operator (remainder from division)
+    // 1 % 5 = 1
+
+    cout << 1 % 5 << endl;
+
+    //incrementation - increase by 1
+    //decrementation - decrease by 1
+
+    */
+/*
+    int c = 1;
+    //c = c + 1;
+    c+=6; //the same as c = c + 1;
+
+    cout << c << endl;
+
+    //    /=   -=    %=    *=
+*/
+
+/*
+    int d = 1;
+    cout << d++ << endl;    // d++ POSTincrementation
+    cout << d << endl;
+    cout << ++d << endl;    // ++d PREincrementation
+    cout << d << endl;
+*/
+
+    int e = 1;
+    cout << e++ << endl; // 1
+    cout << e << endl;  // 2
+    cout << ++e << endl; // 3
+    cout << e << endl;  // 3
+}
