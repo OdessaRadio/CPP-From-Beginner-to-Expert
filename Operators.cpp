745b2ff467da2f8199c1af57bccd4dcf6c3f69f9
OdessaRadio <alexander_lyashko@hotmail.com>
2020-09-18 22:31:49 +0300

    Bitwise operators

--- Operators.cpp
+++ Operators.cpp
@@ -5,26 +5,73 @@ using namespace std;
 
 main()
 {
-    //Ex.1
+    /*
+        0
+        1
+        0101 0110
 
-    int minVal, valueToCheck, maxVal;
-    //bool isGreaterEqualMinNumber, isLowerEqualMaxNumber;
-    bool isWithinInterval;
+        126 = 1*10^2 + 2*10^1 + 6*10^0 = 100 + 20 + 6 = 126
 
-    cout << "Minimum value: ";
-    cin >> minVal;
-    cout << "Maximum value: ";
-    cin >> maxVal;
-    cout << "Value to check: ";
-    cin >> valueToCheck;
+        1 0 1 0 = 1*2^3 + 0*2^2 + 1*2^1 + 0*2^0 = 8 + 0 + 2 + 0 = 10
 
-    //isGreaterEqualMinNumber = (valueToCheck >= minVal);
-    //isLowerEqualMaxNumber = (valueToCheck <= maxVal);
-    isWithinInterval = (valueToCheck >= minVal) && (valueToCheck <= maxVal);
+        1 0 1 0 = 1*2^3 + 1*2^1 = 10
 
-    cout << "Is the value " << valueToCheck << " contained in the interval from " << minVal <<
-            " to " << maxVal << " ? " << isWithinInterval << endl ;
-    //cout << "Is the value " << valueToCheck << " lower ot equal to " << maxVal << " ? " << isLowerEqualMaxNumber << endl;
-    system ("pause");
+        1 0 1 1 0 0 = 1*2^2 + 1*2^3 + 1*2^5 = 44
+    */
+    /*
+        Bitwise AND - &
+        Bitwise OR - |
+        Bitwise NOT - ~ (tilde)
+        Bitwise XOR - ^ (caret) eXclusive OR
+        Bitwise left shift <<
+        Bitwise right shift >>
+    */
 
+    cout << (10 & 2) << endl;
+
+    /*
+    1 0 1 0 = 10
+    0 0 1 0 = 2
+    - - - -
+    0 0 1 0 = 2
+    */
+
+    cout << ( 10 | 2 ) << endl;
+
+    /*
+    1 0 1 0 = 10
+    0 0 1 0 = 2
+    - - - -
+    0 0 1 0 = 2
+    */
+
+    cout << (10 ^ 2) << endl;
+
+    /*
+    1 0 1 0 = 10
+    0 0 1 0 = 2
+    - - - -
+    1 0 0 0 =
+    */
+
+    cout << (~10) << endl;
+
+    /*
+    00000(0) 1 0 1 0
+    11111(1) 0 1 0 1
+    */
+
+    cout << (10 << 1) << endl;
+
+    /*
+      1 0 1 0 = 10
+    1 0 1 0 0 = 20
+    */
+
+    cout << (10 >> 1) << endl;
+
+    /*
+    1 0 1 0 = 10
+    0 1 0 1 = 5
+    */
 }
