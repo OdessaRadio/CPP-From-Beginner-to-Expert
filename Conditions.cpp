2321b17198dc6442d71c8aeac6f43a8eb4074d61
OdessaRadio <alexander_lyashko@hotmail.com>
2020-09-21 16:23:19 +0300

    Conditional statements

---
+++ Conditions.cpp
@@ -0,0 +1,57 @@
+# include <iostream>
+
+using namespace std;
+
+main()
+{
+    int a = 10, b = 10;
+
+    if ( a > b )
+    {
+        cout << "a > b" << endl;
+    }
+    else if ( a < b )
+    {
+        cout << "a < b" << endl;
+    }
+
+    else
+    {
+        cout << " a = b " << endl;
+    }
+/*
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+*/
+/*
+    if (a > b)
+        cout << a << " > " << b << endl;
+*/
+/*
+    if (4 > 7)
+    {
+        cout << "something\n";
+    }
+    else
+    {
+        cout << "test\n";
+    }
+
+    if (1)
+    {
+        cout << "The condition is not equal to ZERO\n";
+    }
+
+    if (3 < 5 || 4 > 6 && 10 == 9)
+    {
+        cout << "Conditional 1\n";
+    }
+
+*/
+
+
+
+
+}
