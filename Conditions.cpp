37e158f4ff6dd81a9f78b43553fbd547ef9a58c5
OdessaRadio <alexander_lyashko@hotmail.com>
2020-09-23 11:29:56 +0300

    Switch

--- Conditions.cpp
+++ Conditions.cpp
@@ -5,108 +5,26 @@ using namespace std;
 
 main()
 {
-//Ex.1
-    /*
-    int minVal, valueToCheck, maxVal;
-    //bool isGreaterEqualMinNumber, isLowerEqualMaxNumber;
-    bool isWithinInterval;
+    char x = 'r';
 
-    cout << "Minimum value: ";
-    cin >> minVal;
-    cout << "Maximum value: ";
-    cin >> maxVal;
-    cout << "Value to check: ";
-    cin >> valueToCheck;
-
-    if (valueToCheck >= minVal && valueToCheck <= maxVal)
+    switch (x)
     {
-        cout << "The number " << valueToCheck << " belongs to the interval from " << minVal << " to "
-            << maxVal << endl;
-    }
-    else
-    {
-        cout << "The number " << valueToCheck << " doesn't belong to the interval from " << minVal << " to "
-            << maxVal << endl;
+        case 0:
+                cout << "switched value is equal to 0" << endl;
+                break;
+        case 25:
+                cout << "switched value is equal to 25" << endl;
+                break;
+        case 50:
+                cout << "switched value is equal to 50" << endl;
+                break;
+        case 'd':
+                cout << "switched value is equal to 'd'" << endl;
+                break;
+        default:
+                cout << "value of the switched variable is not equal to any case" << endl;
+    cout << x << endl;
     }
 
-    system ("pause");
-    */
 
-//Ex. 2
-    /*
-    int minVal, valueToCheck, maxVal;
-    //bool isGreaterEqualMinNumber, isLowerEqualMaxNumber;
-    bool isWithinInterval;
-
-    cout << "Minimum value: ";
-    cin >> minVal;
-    cout << "Maximum value: ";
-    cin >> maxVal;
-
-    if (maxVal > minVal)
-    {
-        cout << "Value to check: ";
-        cin >> valueToCheck;
-
-        bool isContained = (valueToCheck >= minVal) && (valueToCheck <= maxVal);
-
-        if (isContained)
-        {
-            cout << "The number " << valueToCheck << " belongs to the interval from " << minVal << " to "
-                << maxVal << endl;
-        }
-        else
-        {
-            cout << "The number " << valueToCheck << " doesn't belong to the interval from " << minVal << " to "
-                << maxVal << endl;
-        }
-
-    }
-    else
-    {
-        cout << "Error. Maximum value cannot be lower than the minimum value" << endl;
-    }
-    */
-
-//Ex. 3
-    int productSelection;
-
-    cout << "Select the product: input appropriate number." << endl;
-    cout << "1. Cola" << endl;
-    cout << "2. Pepsi" << endl;
-    cout << "3. Fanta" << endl;
-    cout << "4. Sprite" << endl;
-    cout << "5. Mirinda" << endl;
-    cout << "6. 7Up" << endl;
-
-    cin >> productSelection;
-
-    if (productSelection == 1)
-    {
-        cout << "Your have chosen a Cola" << endl;
-    }
-    else if (productSelection == 2)
-    {
-        cout << "Your have chosen a Pepsi" << endl;
-    }
-    else if (productSelection == 3)
-    {
-        cout << "Your have chosen a Fanta" << endl;
-    }
-    else if (productSelection == 4)
-    {
-        cout << "Your have chosen a Sprite" << endl;
-    }
-    else if (productSelection == 5)
-    {
-        cout << "Your have chosen a Mirinda" << endl;
-    }
-    else if (productSelection == 6)
-    {
-        cout << "Your have chosen a 7Up" << endl;
-    }
-    else
-    {
-        cout << "Error. Invalid choice." << endl;
-    }
 }
