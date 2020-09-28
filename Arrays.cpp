a64600a70abbf0e61292177c379dc27b913b7cd5
OdessaRadio <alexander_lyashko@hotmail.com>
2020-09-28 11:27:56 +0300

    Arrays

--- Arrays.cpp
+++ Arrays.cpp
@@ -2,8 +2,35 @@
 
 using namespace std;
 
-
+//ARRAYS
 main()
 {
+    const int n = 100;
+
+    int x[n];
+    int a;
+//    int testArray[4] = {1,2,3,4};
+//
+//    cout << &(testArray[0]) << endl;
+//    cout << &(testArray[1]) << endl;
+//    cout << &(testArray[2]) << endl
+
+    int testArray[4]; //type array name [number of elements]
+    testArray[0] = 10;
+    testArray[1] = 50;
+    testArray[2] = 256;
+    testArray[3] = 128;
+
+    cout << testArray[0] << " address is: " << &testArray[0] << endl;
+    cout << testArray[1] << " address is: " << &testArray[1] << endl;
+    cout << testArray[2] << " address is: " << &testArray[2] << endl;
+    cout << testArray[3] << " address is: " << &testArray[3] << endl;
+
+    cout << testArray[3] << " address is: " << testArray << endl;
+
+    if (&testArray[0]==testArray)
+        cout << "True";
+
+
 
 }
