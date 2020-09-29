4166cf2ad5ba4c1f4b81a39ed627ce3e1f7b082d
OdessaRadio <alexander_lyashko@hotmail.com>
2020-09-29 16:50:58 +0300

    Continue/break

--- Loops.cpp
+++ Loops.cpp
@@ -2,60 +2,38 @@
 
 using namespace std;
 
-//Nested loops
+//break and continue
 main ()
 {
-//Ex.1
-//Setting up an array
-    char array[3][3];
-    char value = '1';
-    int xCoord, yCoord;
-    bool symbol = true;
-    char playerMark;
-
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-        {
-            array[i][j] = value;
-            value++;
-        }
-//Ex.2
-while (true)
+/*
+    for (int i = 1; i <= 10; i++) //amount of rown
     {
-        if (symbol)
+        if (i==5)
+            continue;
+
+        for (int j = 1; j <= 10; j++) //amount of columns
         {
-            playerMark = 'X';
+            if (j==5)
+                continue;
+            cout.width(4);
+            cout << i * j;
         }
-        else
+        cout << endl;
+    }
+*/
+    for (int i = 1, j = 1; i <= 10; i++)
+    {
+        cout.width(4);
+        cout << i * j;
+        if (i==10)
         {
-            playerMark = 'O';
+            j++;
+            i = 0;
+            cout << endl;
         }
+        if (j == 10 + 1)
+            break;
 
-        //Printing part
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                cout.width(3);
-                cout << array[i][j];
-            }
-            cout << endl << endl;
-        }
 
-        do
-        {
-            cout << "Select X for " << playerMark << " ";
-            cin >> xCoord;
-            cout << "Select Y for " << playerMark << " ";
-            cin >> yCoord;
-        } while ((xCoord >= 4)&&(yCoord >= 4));
-
-        if ((array[yCoord-1][xCoord-1] != 'X') && (array[yCoord-1][xCoord-1] != 'O'))
-            {
-            array[yCoord-1][xCoord-1] = playerMark;
-
-            }
-        system ("cls");
-        symbol = !symbol;
     }
 }
