00872f0c11a552e7119cd3061ce1ca88dba3063d
OdessaRadio <alexander_lyashko@hotmail.com>
2020-09-30 14:36:20 +0300

    Functions

--- Functions.cpp
+++ Functions.cpp
@@ -1,35 +1,61 @@
 #include <iostream>
 
 using namespace std;
-/* scope / range of variables */
+/* functions / methods */
 
 int globalVariable;
 
+void welcome(); //declaration of the function
+bool isNumber(string); // no need to declare the name in parameters, only type
+void enterName();
+double add(double a, double b) {return a + b;}
+
+void changeValueTo10 (int x)
+{
+    //int x = a
+    x = 10;
+}
+
 main()
 {
-/*
-    int localVariable;
+    //welcome();
+    //enterName();
+    //cout << add(3,5) << endl;
 
-    cout << "value of globalVariable is " << globalVariable << endl;
-    cout << "value of localVariable is " << localVariable << endl;
+    int a = 5;
+    changeValueTo10(a);
+    cout << a;
 
-    int a = 10;
-    if (a==10)
+    return 0;
+}
+
+bool isNumber(string tmp)
+{
+    if (tmp[0]=='O')
+        return false;
+
+    for (int i = 0; i < tmp.length(); i++)
     {
-        int result = a * 10;
-        cout << result << endl;
+        if (!(tmp[i] >= 48 && tmp[i] <= 57))
+            return false;
     }
-*/
-    int nr;
-    int result = 0;
-    int i = 0;
-    for(; i <3; i++) // 3,2,3,
-    {
-        cout << "Enter " << (i+1) << " number" << endl;
-        cin >> nr;
-        result += nr;
-    }
-    cout << result << endl;
-    cout << "We added " << i << " numbers" << endl;
+    return true;
+}
 
+void welcome()
+{
+    cout << "Hello, welcome to our program" << endl;
+}
+
+void enterName()
+{
+    string tmp;
+
+    cout << "Enter the number: " << endl;
+    cin >> tmp;
+
+    if (isNumber(tmp))
+        cout << "Number is entered properly" << endl;
+    else
+        cout << "Number was not entered properly" << endl;
 }
