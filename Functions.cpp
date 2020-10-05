2923ca284a719c28dea2220186e985e60e0a8ad4
OdessaRadio <alexander_lyashko@hotmail.com>
2020-10-05 13:33:59 +0300

    Overloading functions. Exercises

--- Functions.cpp
+++ Functions.cpp
@@ -2,32 +2,106 @@
 typedef double dl;
 
 using namespace std;
+
 /* overloading functions */
-int power (int, int);
-double power (double ,int);
+
+/*
+//Ex.1
+
+void variableType (int);
+void variableType (double);
+void variableType (string);
 
 main ()
 {
-    cout << power(2.7,4) << endl;
+int intNumber = 10;
+double doubleNumber = 45.6;
+string  stringNumber = "Test string";
+
+variableType(intNumber);
+variableType(doubleNumber);
+variableType(stringNumber);
+}
+
+void variableType (int a)
+{
+   cout << a << " this variable was an integer" << endl;
+}
+void variableType (double a)
+{
+   cout << a << " this variable was an double" << endl;
+
+}
+void variableType (string a)
+{
+   cout << "\"" << a << "\"" << " this variable was an string" << endl;
+
+}
+*/
+
+//Ex.2
+
+double addition (double, double);
+double addition (double, double, double);
+double addition (double, double, double, double);
+double addition (double, double, double, double, double);
+
+main ()
+{
+    int numberOfNumbers;
+    double numberArray[5] = {0};
+    double result;
+
+    do
+    {
+    cout << "Enter number of numbers to be added: ";
+    cin >> numberOfNumbers;
+    } while (numberOfNumbers < 2 || numberOfNumbers > 5);
+
+
+    for (int i = 1; i <= numberOfNumbers; i++)
+    {
+        cout << "Enter number " << i << " :";
+        cin >> numberArray[i-1];
+    }
+
+    switch (numberOfNumbers)
+    {
+        case 2:
+            result = addition(numberArray[0],numberArray[1]);
+            break;
+        case 3:
+            result = addition(numberArray[0], numberArray[1], numberArray[2]);
+            break;
+        case 4:
+            result = addition(numberArray[0], numberArray[1], numberArray[2],numberArray[3]);
+            break;
+        case 5:
+            result = addition(numberArray[0], numberArray[1], numberArray[2],numberArray[3],numberArray[4]);
+            break;
+    }
+
+    cout << "Result is: " << result << endl;
+
     return 0;
 }
 
-int power (int b, int e)
+double addition (double n1, double n2)
 {
-    int tmp = b;
-    while (e-- >1)
-    {
-        b *= tmp;
-    }
-    return b;
+    return n1+n2;
 }
 
-double power (double b, int e)
+double addition (double n1, double n2, double n3)
 {
-    int tmp = b;
-    while (e-- >1)
-    {
-        b *= tmp;
-    }
-    return b;
+    return n1+n2+n3;
+}
+
+double addition (double n1, double n2, double n3, double n4)
+{
+    return n1+n2+n3+n4;
+}
+
+double addition (double n1, double n2, double n3, double n4 , double n5)
+{
+    return n1+n2+n3+n4+n5;
 }
