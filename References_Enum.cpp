f03c22ef62b4f1ea4adc52a1a901e3566d5f3592
OdessaRadio <alexander_lyashko@hotmail.com>
2020-10-07 13:56:57 +0300

    Reference variables

--- References_Enum.cpp
+++ References_Enum.cpp
@@ -1,49 +1,36 @@
 #include <iostream>
 
-/* Enumeration */
+/*reference variable*/
 
 using namespace std;
 
-//      by dafault  0  1   2   3  4  5   6
-enum dayOfWeek {M = 1, TU, W, TH, F, SA, SN}; //dayOfWeek is a new data type
-// When assigning M = 1 we change the enumeration start index
-
-string getDay(dayOfWeek);
 
 int main ()
 {
-    int i;
-//    dayOfWeek d = W;
-//    cout << d << endl;
-//    cout << dayOfWeek (TU) << endl;
-//    cout << dayOfWeek (W) << endl;
-    cout << "Enter number of day\n";
-    cin >> i;
-    cout << getDay(dayOfWeek(i)) << endl; //calling the week day by number
+    string name2 = "Maria";
+    string name = "Agnes";
+
+    cout << "Value of variable: " << name << " address of the variable " << &name << endl;
+
+    const string &nr_6 = name; //&nr_6 is a reference for the name
+    //nr_6 = name2;
+
+    cout << "Value of variable: " << nr_6 << " address of the variable " << &nr_6 << endl;
+
+    name = "Scott";
+
+    cout << "Value of variable: " << name << " address of the variable " << &name << endl;
+    cout << "Value of variable: " << nr_6 << " address of the variable " << &nr_6 << endl;
+
+    /*
+    Rules:
+     - Reference variable must be initialized when created
+     - Ref. variable can't change associations / connections
+     - Ref. variable must be the same type as associated variable
+    */
+
+
 
 
     return 0;
-}
-
-string getDay(dayOfWeek d)
-{
-    switch (d)
-    {
-    case M:
-        return "Monday";
-    case TU:
-        return "Tuesday";
-    case W:
-        return "Wednesday";
-    case TH:
-        return "Thursday";
-    case F:
-        return "Friday";
-    case SA:
-        return "Saturday";
-    case SN:
-        return "Sunday";
-    default:
-        cout << "You typed something wrong" << endl;
-    }
 }
