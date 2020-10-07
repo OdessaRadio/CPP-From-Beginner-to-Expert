0109186777cfa7c723053b0128dbf30a39e8d4ad
OdessaRadio <alexander_lyashko@hotmail.com>
2020-10-07 09:40:09 +0300

    cin.ignore / cin.clear

--- Functions.cpp
+++ Functions.cpp
@@ -1,135 +1,31 @@
 #include <iostream>
-#include <limits>
 
-/*
-#define PI 3.14
-
-typedef double dl;
-
-//validating data
 
 using namespace std;
 
-void initMenu();
-void menuDecision(int);
+int main(){
+    string name;
+    cout << "Give me your name and surname:"<<endl;
+    cin >> name;
 
-double areaCircle(double r);
-double areaSquare(double a);
-double areaRectangle(double a, double b);
-double areaTriangle(double a, double h);
-
-int main ()
-{
-    system ("cls");
-
-    int choice;
-    char cont;
-    do
+    int age;
+    cout << "Give me your age:" <<endl;
+    if (cin >> age)
     {
-    initMenu();
-    cin >> choice;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Good\n";
+    }
+    else cout << "No Good\n";
+//    cout << cin.rdstate() << endl;
+//    cin.clear(); //new line is here :-)
+//    cout << cin.rdstate()<< endl;  //new line is here :-)
 
-    menuDecision(choice);
+    if (cout << 1+1 << endl){
+        cout << "Good" << endl;
+    }
 
-    do
-        {
-        cout << "Do you want to continue? (Y/N)" << endl;
-        cin >> cont;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-        } while (cont != 'y' && cont != 'Y' && cont != 'N' && cont != 'n');
-
-    } while (cont == 'y'|| cont == 'Y');
+    }
 
 
 
-    return 0;
-}
 
-void initMenu()
-{
-    cout << "Enter option:" << endl;
-    cout << "1. Circle" << endl;
-    cout << "2. Square" << endl;
-    cout << "3. Rectangle" << endl;
-    cout << "4. Triangle" << endl;
-}
-
-void menuDecision(int choise)
-{
-    double r,a,b,h;
-
-    switch (choise)
-        {
-        case 1:
-            cout << "Enter the radius: " << endl;
-            cin >> r;
-            areaCircle (r);
-            break;
-        case 2:
-            cout << "Enter the side of a square: " << endl;
-            cin >> a;
-            areaSquare (a);
-            break;
-        case 3:
-            cout << "Enter the width and height of a rectangle: " << endl;
-            cin >> a >> b;
-            areaRectangle(a, b);
-            break;
-        case 4:
-            cout << "Enter the base and height of triangle: " << endl;
-            cin >> a >> h;
-            areaTriangle(a ,h);
-            break;
-        default:
-            cout << "No choice made" << endl;
-        }
-}
-
-double areaCircle (double r)
-{
-    double result = PI * r *r;
-    cout << "The area of the circle is: " << result << endl;
-    return result;
-}
-
-double areaSquare(double a)
-{
-    double result = a*a;
-    cout << "The area of the square is: " << result << endl;
-    return result;
-}
-
-double areaRectangle(double a, double b)
-{
-    double result = a*b;
-    cout << "The area of the rectangle is: " << result << endl;
-    return result;
-}
-
-double areaTriangle(double a, double h)
-{
-    double result = a*h;
-    cout << "The area of the triangle is: " << result << endl;
-    return result;
-}
-
-*/
-
-using namespace std;
-
-int main ()
-{
-    int number;
-    string mystr;
-
-    cout << "Enter the number: ";
-
-    cin >> number;
-    cin.ignore('a');
-
-    cout << number;
-
-    return 0;
-}
