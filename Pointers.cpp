98e2ea9da5d33041b8fe6907555be0f937d46616
OdessaRadio <alexander_lyashko@hotmail.com>
2020-10-08 17:34:47 +0300

    Strings and pointers on the Char type

--- Pointers.cpp
+++ Pointers.cpp
@@ -1,52 +1,58 @@
 #include <iostream>
 
 using namespace std;
-/*
-// Ex.1
-int main ()
+
+//* Strings and a pointer on a char */
+
+int main()
 {
-    int statical;
 
-    int *statically = &statical; // *statically - pointer to statical
-    int *dynamically = new int; // *dynamically - pointer to dynamically allocated memory 4 bytes
+    string text = "sample text";// the last symbol is \0
+    /*
+    for (int i = 0; i < text.length();i++){
+        cout << text[i] << endl;
+    }
 
-    *statically = 10;
-    *dynamically = 15;
+    cout << "\n\n\n";
 
-    cout << "The value of the statically allocated variable: " << *statically << ", and the address: " << statically << endl;
-    cout << "The value of the dynamically allocated variable: " << *dynamically << ", and the address: " << dynamically << endl;
+    char ch = text[0];
+    cout << ch << endl;
 
-    delete dynamically;
+    char characters[] = "1234"; // [] compiler will give the size automatically
+
+    cout << characters[0] << endl;
+    cout << *(characters) << endl;
+
+    char *p = characters;
+
+    cout << p[1] << endl;
+    cout << *(p+1) << endl;
+*/
+    const char *text2 = text.c_str(); // c_str() is a method to convert string to char
+    cout << text2 << endl;
+
+    char array_1[] = "here is a text";
+    string test = array_1;
+    cout << array_1 << endl;
+
+    const char * a = "this is a text 43243243"; // this is a const because created on compiling. We can't change *a
+    cout << a[0] << endl;
+
+    char b[] = "sdasdsda";
+    cout << b << endl;
+
+    b[0] = 'g';
+    cout << b << endl;
+
+    char * const dynamic_array = new char [50];
+    dynamic_array [0] = 'f';
+    cout << dynamic_array << endl;
+    delete [] dynamic_array;
+
+
+    string array_of_string[3];
+    cout << array_of_string[0] << endl;
+    cout << array_of_string[1] << endl;
 
     return 0;
 }
-*/
-//Ex.2
-int main ()
-{
-
-    int arraySize;
-
-    cout << "Input the size of the dynamic array you want to create: ";
-    cin >> arraySize;
-
-    int *dynamicArray = new int[arraySize];
-
-
-    for (int i = 0; i < arraySize; i++)
-    {
-        cout << "Input " << i+1 << " number: ";
-        cin >> *(dynamicArray+i);
-    }
-
-    for (int i = 0; i < arraySize; i++)
-    {
-        cout << "Number " << i+1 << ": " << *(dynamicArray+i) << endl;
-    }
-
-    delete []dynamicArray;
-
-return 0;
-}
-
-
