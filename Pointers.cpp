f9ec65aacb3bd50648bd88395c9b068b754dba6e
OdessaRadio <alexander_lyashko@hotmail.com>
2020-10-08 19:02:38 +0300

    Functions and pointers

--- Pointers.cpp
+++ Pointers.cpp
@@ -1,44 +1,84 @@
 #include <iostream>
 
 using namespace std;
-
-//* Strings and a pointer on a char */
-//Ex.1, Ex.2
 /*
-int main()
-{
-    string var = "abcdefghijklmnopqrstuvwxyz";
-    for (int i = 0; i < var.length(); i++)
-        cout << var[i];
-
-    return 0;
-}
-*/
-//Ex.3
+void multiplyBy (int *, int);
 
 int main() {
 
-int number;
+    int a = 10;
+    multiplyBy (&a, 5);
+    cout << a << endl;
 
-cout << "How many characters would you like to input?\n";
-cin >> number;
-
-char * myList = new char [number];
-
-for (int i = 0; i<number; i++){
-    cout << "Enter " << i+1 << " number: ";
-    cin >> myList[i];
+    return 0;
 }
 
-for (int i = 0; i<number; i++){
-    cout << *(myList+i) << endl;
-    cout << myList << endl;
+
+void multiplyBy (int *var, int amount)// *var = &a
+{
+*var *= amount;
+
+}
+*/
+/*
+int *multiplyBy (int *, int); //
+
+int main() {
+
+    int a = 10;
+    int *b = multiplyBy (&a, 5); // *b = var. Function returns the address
+   // *b = 100;
+    cout << a << endl;
+    cout << b << endl;
+
+
+
+    return 0;
 }
 
-//string myString = myList;
-//cout << myString;
+
+int *multiplyBy (int *var, int amount)// *var = &a
+{
+*var *= amount;
+
+return var;
+
+}
+*/
+int *multiplyBy (int *, int); //
+void multiplyArrayBy (int *, int, int); // int * = &myArray[0]
+
+main (){
+
+    int myArray[10];
+    int arraySize = sizeof(myArray)/sizeof(myArray[0]);
+    cout << "arraySize is: " << arraySize << endl; // 10 * 4 bytes = 40 bytes
 
 
-delete [] myList;
-return 0;
+    for (int i = 0; i < arraySize; i ++){
+        myArray[i] = i;
+        //cout << "myArray [" << i << "] = " << myArray[i] << endl;
+    }
+
+    multiplyArrayBy (myArray, 5, arraySize);
+
+    for (int i = 0; i < arraySize; i ++){
+        cout << "myArray [" << i << "] = " <<myArray[i] << endl;
+    }
+
+    return 0;
+}
+
+
+int *multiplyBy (int *var, int amount)// *var = &a
+{
+*var *= amount;
+return var;
+}
+
+void multiplyArrayBy (int *myArray, int amount, int arraySize){
+    while (--arraySize)
+        cout << arraySize << endl;
+        myArray[arraySize] *= amount;
+
 }
