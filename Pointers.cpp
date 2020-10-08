2c79c367c26fc708b645ad47c3d5ba0a35286275
OdessaRadio <alexander_lyashko@hotmail.com>
2020-10-08 16:25:56 +0300

    Dynamic allocation of memory.  Exercises

--- Pointers.cpp
+++ Pointers.cpp
@@ -1,102 +1,52 @@
 #include <iostream>
+
 using namespace std;
-/*Dynamic allocation of memory*/
-
-
-int main()
+/*
+// Ex.1
+int main ()
 {
-    /*
-    int array[100];
-    int * p_1 = &array[0];
-    array[0] = 99;
-    array[100] = 88;
-    cout << *p_1 << endl;
-    cout << *(p_1 + 100) << endl;
-    */
-/*
-    int amount;
+    int statical;
 
-    cout << "How many numbers would you like to store in the memory? " << endl;
-    cin >> amount;
+    int *statically = &statical; // *statically - pointer to statical
+    int *dynamically = new int; // *dynamically - pointer to dynamically allocated memory 4 bytes
 
-    int *p = new (nothrow) int[amount]; // pointer to the first element of array
+    *statically = 10;
+    *dynamically = 15;
 
-    if (p!=NULL){
+    cout << "The value of the statically allocated variable: " << *statically << ", and the address: " << statically << endl;
+    cout << "The value of the dynamically allocated variable: " << *dynamically << ", and the address: " << dynamically << endl;
 
-    cout << p << endl;
-
-        for(int i = 0; i < amount-1; i ++){
-            cout << p[i] << endl;
-        }
-    }
-    else
-        cout << "Too many memory"<< endl;
-
-    int y = 0;
-    for(int i = 0; i < amount; i ++){
-        y = i;
-        cout << "i = " << i << " y = " << y << endl;
-        cout << "Enter the " << (y+1) << " number :";
-        cin >> p[i];
-    }
-
-    for(int i = 0; i < amount; i ++){
-        cout << "p[" << i << "] = " << p[i] << endl;
-    }
-
-    delete p;
-*/
-
-
-//    cout << new int << endl; // new int - is an address
-//    cout << *(new int) << endl;
-
-/*
-    int var = 250;
-
-
-    {
-    int *p = new int;
-
-    *p = 50;
-
-    cout << "*p = " << *p << endl;
-    cout << "p = " << p << endl;
-
-    delete p; // delete - releasing the pointer. Deleting variable address from the pointer
-    p = NULL;
-
-    p = &var;
-
-    *p = 300;
-
-    if (p!=NULL)
-        {
-            *p = 100;
-            cout << *p << endl;
-        }
-
-    cout << "*p = " << *p << endl;
-    cout << "p = " << p << endl;
-    }
-
-
-    //array[0] = 500;
-
-    cout << "var" << endl;
-    cout << var << endl;
-*/
-    int *p = new int; // address reservation
-    cout << p << endl;
-    delete p;
-
-    p = new int; // address reservation
-    cout << p << endl;
-    delete p;
+    delete dynamically;
 
     return 0;
 }
+*/
+//Ex.2
+int main ()
+{
+
+    int arraySize;
+
+    cout << "Input the size of the dynamic array you want to create: ";
+    cin >> arraySize;
+
+    int *dynamicArray = new int[arraySize];
 
 
+    for (int i = 0; i < arraySize; i++)
+    {
+        cout << "Input " << i+1 << " number: ";
+        cin >> *(dynamicArray+i);
+    }
+
+    for (int i = 0; i < arraySize; i++)
+    {
+        cout << "Number " << i+1 << ": " << *(dynamicArray+i) << endl;
+    }
+
+    delete []dynamicArray;
+
+return 0;
+}
 
 
