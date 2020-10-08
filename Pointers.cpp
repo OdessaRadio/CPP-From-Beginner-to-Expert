2d45fca9543bb88e5686f5d9850756cfba1bfba4
OdessaRadio <alexander_lyashko@hotmail.com>
2020-10-08 18:08:42 +0300

    Strings and a pointer on a char type. Exercises

--- Pointers.cpp
+++ Pointers.cpp
@@ -3,56 +3,42 @@
 using namespace std;
 
 //* Strings and a pointer on a char */
-
+//Ex.1, Ex.2
+/*
 int main()
 {
-
-    string text = "sample text";// the last symbol is \0
-    /*
-    for (int i = 0; i < text.length();i++){
-        cout << text[i] << endl;
-    }
-
-    cout << "\n\n\n";
-
-    char ch = text[0];
-    cout << ch << endl;
-
-    char characters[] = "1234"; // [] compiler will give the size automatically
-
-    cout << characters[0] << endl;
-    cout << *(characters) << endl;
-
-    char *p = characters;
-
-    cout << p[1] << endl;
-    cout << *(p+1) << endl;
-*/
-    const char *text2 = text.c_str(); // c_str() is a method to convert string to char
-    cout << text2 << endl;
-
-    char array_1[] = "here is a text";
-    string test = array_1;
-    cout << array_1 << endl;
-
-    const char * a = "this is a text 43243243"; // this is a const because created on compiling. We can't change *a
-    cout << a[0] << endl;
-
-    char b[] = "sdasdsda";
-    cout << b << endl;
-
-    b[0] = 'g';
-    cout << b << endl;
-
-    char * const dynamic_array = new char [50];
-    dynamic_array [0] = 'f';
-    cout << dynamic_array << endl;
-    delete [] dynamic_array;
-
-
-    string array_of_string[3];
-    cout << array_of_string[0] << endl;
-    cout << array_of_string[1] << endl;
+    string var = "abcdefghijklmnopqrstuvwxyz";
+    for (int i = 0; i < var.length(); i++)
+        cout << var[i];
 
     return 0;
+}
+*/
+//Ex.3
+
+int main() {
+
+int number;
+
+cout << "How many characters would you like to input?\n";
+cin >> number;
+
+char * myList = new char [number];
+
+for (int i = 0; i<number; i++){
+    cout << "Enter " << i+1 << " number: ";
+    cin >> myList[i];
+}
+
+for (int i = 0; i<number; i++){
+    cout << *(myList+i) << endl;
+    cout << myList << endl;
+}
+
+//string myString = myList;
+//cout << myString;
+
+
+delete [] myList;
+return 0;
 }
