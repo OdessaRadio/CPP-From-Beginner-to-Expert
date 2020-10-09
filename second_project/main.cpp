610371a0932818e3950000845e1ea2da24b144c3
OdessaRadio <alexander_lyashko@hotmail.com>
2020-10-09 14:19:06 +0300

    Preprocessor directives. Multi - file project.

---
+++ second_project/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "second_lib.h"
+
+using namespace std;
+
+#define PI 3.14
+
+/* Preprocessing instructions */
+
+
+int main()
+{
+    showHelp();
+
+    cout << PI << endl;
+    cout << a << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// some change in code somewhere
+
+
---
+++ second_project/second_lib.h
@@ -0,0 +1,12 @@
+#ifndef second_lib_H_INCLUDED //if not defined
+#define second_lib_H_INCLUDED // define
+
+
+
+extern int a; // external (outside). We do not reserved memory for variable a
+//we should have variable a defined somewhere else
+
+extern void showHelp();
+
+
+#endif // second_lib_H_INCLUDED // end if
