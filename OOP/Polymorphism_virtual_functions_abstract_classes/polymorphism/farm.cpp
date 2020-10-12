daf9591545c3c38fc0d425c4a73190b5e7f0f3b4
OdessaRadio <alexander_lyashko@hotmail.com>
2020-10-12 15:10:08 +0300

    Polymorphism, virtual functions, abstract classes

---
+++ OOP/Polymorphism_virtual_functions_abstract_classes/polymorphism/farm.cpp
@@ -0,0 +1,47 @@
+#include "farm.h"
+
+Animal::Animal()
+{
+    voice = "default value";
+}
+
+Animal::~Animal()
+{
+
+}
+
+//
+Dog::Dog()
+{
+    sign = "D";
+    voice = "hau hau hau";
+}
+
+Dog::~Dog()
+{
+
+}
+
+//
+Cat::Cat()
+{
+    sign = "CO";
+    voice = "meow";
+}
+
+Cat::~Cat()
+{
+
+}
+
+//
+Cow::Cow()
+{
+    sign = "C";
+    voice = "muuu";
+}
+
+Cow::~Cow()
+{
+
+}
