9eb6e084c3a7c42668051a42078355b494ec3af7
OdessaRadio <alexander_lyashko@hotmail.com>
2020-10-12 19:30:56 +0300

    Reading position pointer - seekg / tellp

--- FSTREAM/streams.cpp
+++ FSTREAM/streams.cpp
@@ -1,50 +1,59 @@
-#include <fstream>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-
-
 int main()
 {
-
     /*
-        bad() - badbit
-        clear() - clear the state
-        fail() - situation when we try to assign the intiger for the wrong type + bad bit
-        good() - if everything is ok
-        rdstate() - reading the state file
-        eof() - end of file bit. Returns true if the file opened to read reached the end of file
+        tellg - tell get - tell where is the reading pointer
+        seekg - seek get - set reading pointer at specified position
+
+
+        seekg(how_many_bytes_from_the_flag_place, flag);
+
+        possible flags:
+        ios::beg - (begin) set from the begin (default)
+        ios::end - set from the end
+        ios::cur - (current) set from current place
     */
 
     fstream file;
 
-    file.open("sample.txt", ios::in);
+    file.open("sample.txt", ios::in | ios::binary);
+
     if (file.is_open())
     {
         string buffer;
 
+        file.seekg(0, ios::end);
+
+        streampos sizeOfFile = file.tellg();
+
+        file.seekg(0);
+
+        cout << "The size of the file is " << sizeOfFile << " bytes" << endl;
         do
         {
             file >> buffer;
-            cout << buffer;
-        } while (!file.eof());
 
-        cout << endl;
+            cout << buffer << endl;
+        }while (!file.eof());
 
-
-        if( (file.rdstate() ^ ifstream::eofbit) == 0 )
+        if ((file.rdstate() ^ ifstream::eofbit) == 0)
         {
-            cout << "error\n";
             file.clear();
+            cout << file.tellg() << endl;
+            file >> buffer;
+
+            cout << buffer << endl;
+            //set indicator of place in file to some other place
+            // some other operations on file
         }
 
-        cout << file.rdstate() << endl;
     }
     else
-        cout << "The file can't be opened properly";
-
-
+        cout << "The file couldn't be opened properly" << endl;
 
     return 0;
 }
