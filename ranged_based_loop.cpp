ead1034d9c733579bcc87c533997ae883b216f9b
OdessaRadio <alexander_lyashko@hotmail.com>
2020-10-13 11:48:40 +0300

    ranged-based loop - how to prevent making a copy

--- ranged_based_loop.cpp
+++ ranged_based_loop.cpp
@@ -3,23 +3,45 @@
 
 using namespace std;
 
+class Book
+{
+     public:
+     Book()
+     {
+         cout << "constructor is invoked" << endl;
+     }
+
+     Book(const Book &secondBook) { cout <<"some copy constructor instructions" << endl; }
+     string getInfo()
+     {
+        return "Hey I'm book";
+
+     }
+
+};
 int main()
 {
 
-    vector<int> wholeNumbers {20, 10, 4, -3, 25};
+    vector<Book> wholeNumbers {Book(), Book(), Book(), Book(), Book()};
 
     /*
         ranged based for loop C++11 - it loops as long as there are elements within RANGE (scope) of container
     */
 
-    for(auto it = wholeNumbers.begin(); it != wholeNumbers.end(); it++)
+ /*   for(auto it = wholeNumbers.begin(); it != wholeNumbers.end(); it++)
     {
-        auto nr = *it;
+        auto& nr = *it;
+        nr *= 10;
         cout << nr << endl;
     }
+*/
+    for (auto& nr : wholeNumbers)
+    {
+        cout << nr.getInfo() << endl;
+    }
 
-    for (auto nr : wholeNumbers)
-        cout << nr << endl;
+
+
 
 
 
